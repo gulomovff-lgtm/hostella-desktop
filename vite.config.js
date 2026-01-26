5781e393b5aba6d71fd06681f3ea2abac0da8d44
Gulomovff <gulomovff@gmail.com>
2026-01-27 03:03:18 +0500

    Fix base path in vite config

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,5 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   base: './', // <--- ВОТ ЭТА СТРОЧКА САМАЯ ВАЖНАЯ
   plugins: [react()],
+  base: './', // <--- ДОБАВЬТЕ ЭТУ СТРОКУ
 })
