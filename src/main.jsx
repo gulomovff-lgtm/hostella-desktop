581fb96a43fd670d045546c54f7e5dda2ce51ffa
gulomovff-lgtm <gulomovff@icloud.com>
2026-02-28 13:29:52 +0500

    chore: add build timestamp to force fresh deploy

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,10 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { BUILD_TS } from './constants/config.js'
+
+// eslint-disable-next-line no-console
+console.info('[Hostella] build:', BUILD_TS)
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
